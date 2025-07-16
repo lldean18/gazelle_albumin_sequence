9cd67ac6212fa1d88dd09b45bfbe4fa86f694920
Laura Dean <lldean18@gmail.com>
2025-07-16 15:28:25 +0100

    updating to add result of the search

--- 04_identify_gene_region.bash
+++ 04_identify_gene_region.bash
@@ -30,11 +30,24 @@ tblastn -query $wkdir/albumin_protein.fasta \
         -subject $wkdir/bams/clean_bams/ERR7570036_consensus.fasta \
         -out $wkdir/albumin_tblastn.out \
         -outfmt 6 \
-        -evalue 1e-5 \
+        -evalue 1e-10 \
         -num_threads 4
-
 
 
 # deactivate software
 conda deactivate
+
+
+# The second column of the ouput gives the chr name
+# The 9th and 10th columns give the start and end of the match
+# The sheep albumin protein is the best match (unsurprising since it is the most closely related)
+# it must be on the reverse strand bc the start and end are the wrong way around
+# full stretch of sheep match is 16856815 to 16899313 of ENA|CAKJTW010000001|CAKJTW010000001.1 which spans 42,498 bases
+# will take 10,000 bases extra from each side so 16846815 to 16909313
+
+
+
+
+
+
 
