846f3d0784e844c35c33ae7785912ac710881772
Laura Dean <lldean18@gmail.com>
2025-07-17 13:56:16 +0100

    first version of script to predict exon structure

---
+++ 06_predict_exon_structure.bash
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Laura Dean
+# 16/7/25
+# script written for running on the UoN HPC Ada
+
+# script to generate a concensus sequence from reads aligned to a reference
+
+#SBATCH --job-name=predict_exon_structure
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=30g
+#SBATCH --time=48:00:00
+#SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
+
+# set variables
+wkdir=/gpfs01/home/mbzlld/data/gazelle
+albumin_seqs=( /gpfs01/home/mbzlld/data/gazelle/bams/clean_bams/*_albumin_region.fasta )
+
+cd $wkdir
+
+# activate software
+source $HOME/.bash_profile
+#conda create --name exonerate bioconda::exonerate -y
+conda activate exonerate
+
+for seq in ${albumin_seqs[@]}; do
+# predict the exon structure
+exonerate --model protein2genome \
+          --showtargetgff yes \
+          --showalignment no \
+          --query albumin_protein.fasta \
+          --target $seq > ${seq%.*}_exonerate.out
+done
+
+# deactivate software
+conda deactivate
+
